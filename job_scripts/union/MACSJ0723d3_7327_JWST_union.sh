6df05cfdc918bcf9abece5061c6fbfdaafffbbab
Jacqueline McCleary <jmac.ftw@gmail.com>
2024-12-05 17:55:29 -0500

    update job script

--- job_scripts/union/MACSJ0723d3_7327_JWST_union.sh
+++ job_scripts/union/MACSJ0723d3_7327_JWST_union.sh
@@ -4,17 +4,17 @@
 #SBATCH -n 18
 #SBATCH --mem-per-cpu=10g
 #SBATCH --partition=short
-#SBATCH -J MACSJ0723d3_7327_JWST
+#SBATCH -J MACSJ0723_union
 #SBATCH -v
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=j.mccleary@northeastern.edu
-#SBATCH -o slurm_outfiles/MACSJ0723d3_7327_JWST.out
+#SBATCH -o slurm_outfiles/MACSJ0723_b_union.out
 
 
 
 source /work/mccleary_group/miniconda3/etc/profile.d/conda.sh
 
-conda activate sbmcal_139v2
+conda activate sbmcal_139v3
 echo $PATH
 echo $PYTHONPATH
 dirname="slurm_outfiles"
@@ -29,15 +29,17 @@ then
 
  echo "Proceeding with code..."
 
-export OUTDIR='/work/mccleary_group/superbit/union/MACSJ0723d3_7327_JWST/b/out'
-export DATADIR='/work/mccleary_group/superbit/union'
-export CODEDIR='/work/mccleary_group/superbit/superbit-metacal/superbit_lensing'
+export TARGET="MACSJ0723d3_7327_JWST"
+export BAND="b"
+export DATADIR="/work/mccleary_group/superbit/union"
+export OUTDIR="${DATADIR}/${TARGET}/${BAND}/out"
+export CODEDIR="/work/mccleary_group/superbit/superbit-metacal/superbit_lensing"
 
 ## medsmaker
-python $CODEDIR/medsmaker/scripts/process_2023.py MACSJ0723d3_7327_JWST b,g,u $DATADIR -psf_mode=psfex -psf_seed=1134891269 -star_config_dir $CODEDIR/medsmaker/configs 
+python $CODEDIR/medsmaker/scripts/process_2023.py -outdir $OUTDIR $TARGET $BAND $DATADIR -psf_mode=psfex -psf_seed=33876300 -star_config_dir $CODEDIR/medsmaker/configs --meds_coadd
 
 ## metacalibration
-python $CODEDIR/metacalibration/ngmix_fit_superbit3.py $OUTDIR/MACSJ0723d3_7327_JWST_b_meds.fits $OUTDIR/MACSJ0723d3_7327_JWST_b_mcal.fits -outdir=$OUTDIR -n 48 -seed=4125165605 --overwrite
+python $CODEDIR/metacalibration/ngmix_fit_superbit3.py $OUTDIR/"${TARGET}_${BAND}_meds.fits" $OUTDIR/"${TARGET}_${BAND}_mcal.fits" -outdir=$OUTDIR -n 48 -seed=4225165605 --overwrite 
 
 ## shear_profiles
-#python $CODEDIR/shear_profiles/make_annular_catalog.py $DATADIR Abell3411 $OUTDIR/Abell3411_b_mcal.fits $OUTDIR/Abell3411_b_annular.fits -outdir=$OUTDIR --overwrite -cluster_redshift=0.2965 -redshift_cat=/work/mccleary_group/vassilakis.g/bit/real-data-2023/Abell3411/Abell3411_detection_cat_redshits.fits
+python $CODEDIR/shear_profiles/make_annular_catalog.py $DATADIR $TARGET $OUTDIR/"${TARGET}_${BAND}_mcal.fits" $OUTDIR/"${TARGET}_${BAND}_annular.fits" -outdir=$OUTDIR --overwrite -cluster_redshift=0.1689 -redshift_cat=$DATADIR/"catalogs/redshifts/${TARGET}_NED_redshifts.csv"
